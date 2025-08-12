4cc4dbc2b0af90842edb39fe91828341a761701f
Karan Singh Manral <66790855+KaranManral@users.noreply.github.com>
2025-08-12 17:03:06 +0530

    Update sfEmbeddedDeployment.js

--- src/js/sfEmbeddedDeployment.js
+++ src/js/sfEmbeddedDeployment.js
@@ -4,16 +4,7 @@ export function initEmbeddedMessaging() {
 
     // Send data to Salesforce
     embeddedservice_bootstrap.prechatAPI.setHiddenPrechatFields({
-      candidateEmail: "sammy@udp.com",
-      candidateId: "ksdjud23",
-      candidateName: "Sammy Alty",
-      candidatePhone: "0192902920",
-      JobApplicationNumber: "JAR-0007",
-      jobCompanyName: "MIAW",
-      jobApplicationId: "safw213",
-      T_C_Agreed: "true",
-      jobName: "SDE",
-      allowUser: "true",
+      JobApplicationNumber: "JAR-0002"
     });
 
     // // Remove any items from the previous list that you don't want to send
